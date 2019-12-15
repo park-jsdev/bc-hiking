ecd68bbcfecc9b9a6a9c91e51ab0644f4dd5967b
root <jun_park@live.com>
2019-12-15 03:05:41 +0000

    initial commit

--- app.js
+++ app.js
@@ -50,6 +50,11 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function() {
-	console.log("YelpCamp Server Has Started.");
+// app.listen(3000, function() {
+// 	console.log("YelpCamp Server Has Started.");
+// });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
